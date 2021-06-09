577af7a78a946bbc6b05bfd5cb809758fa314fe8
Thilo Ackermann <thilo@ackermann.it>
2021-06-09 17:06:35 +0200

    Initial

---
+++ JuControlDevice/module.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+	class JuControlDevice extends IPSModule
+	{
+		public function Create()
+		{
+			//Never delete this line!
+			parent::Create();
+
+			$this->RequireParent('{4CB91589-CE01-4700-906F-26320EFCF6C4}');
+		}
+
+		public function Destroy()
+		{
+			//Never delete this line!
+			parent::Destroy();
+		}
+
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+		}
+
+		public function Send(string $RequestMethod, string $RequestURL, string $RequestData, int $Timeout)
+		{
+			$this->SendDataToParent(json_encode(['DataID' => '{D4C1D08F-CD3B-494B-BE18-B36EF73B8F43}', "RequestMethod" => $RequestMethod, "RequestURL" => $RequestURL, "RequestData" => $RequestData, "Timeout" => $Timeout]));
+		}
+
+		public function ReceiveData($JSONString)
+		{
+			$data = json_decode($JSONString);
+			IPS_LogMessage('Device RECV', utf8_decode($data->Buffer . ' - ' . $data->RequestMethod . ' - ' . $data->RequestURL . ' - ' . $data->RequestData . ' - ' . $data->Timeout));
+		}
+	}
---
+++ README.md
@@ -0,0 +1,6 @@
+# JuControlConnector
+
+Folgende Module beinhaltet das JuControlConnector Repository:
+
+- __JuControlDevice__ ([Dokumentation](JuControlDevice))  
+	Kurze Beschreibung des Moduls.
