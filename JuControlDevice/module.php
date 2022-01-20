e022ff303c6ab0ab912d53c0c21de0bd6dfdae6f
Thilo Ackermann <thilo@ackermann.it>
2022-01-21 00:42:54 +0100

    fix.

--- JuControlDevice/module.php
+++ JuControlDevice/module.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/../libs/Webclient.php';
-require_once __DIR__ . '/../libs/ConstHelper.php';
+require_once __DIR__ . '/../libs/DebugHelper.php';
 
 	class JuControlDevice extends IPSModule
 	{
